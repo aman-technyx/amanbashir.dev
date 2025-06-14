ff91d2bce98c5686e6acb4d8b65c02bb52ea53c9
aman bashir <44024950+aman-technyx@users.noreply.github.com>
2025-06-14 19:28:10 +0500

    Update configuration for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/amanbashir.dev/',
+  base: '/',
   build: {
     outDir: 'docs'
   }
