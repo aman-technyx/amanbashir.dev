1874db6fd3488d24f42e7152fc28c94ef3095e2a
aman bashir <44024950+aman-technyx@users.noreply.github.com>
2025-08-01 02:45:24 +0500

    add download cv button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,6 +47,12 @@ const Home = ({ theme }) => {
                             <Link to="projects" className="btn btn-outline" smooth={true} duration={500}>
                                 View My Work
                             </Link>
+                            <a href="/files/aman-bashir-cv.pdf" className="btn btn-outline" download>
+                                <span>Download CV</span>
+                                <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
+                                </svg>
+                            </a>
                         </div>
                     </div>
                 </div>
